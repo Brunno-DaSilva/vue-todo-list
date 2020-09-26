01bf9d506b2381581e631bddf8c3bb3df4d13f35
Bruno DaSilva <hi.dasilvab@gmail.com>
2020-09-25 23:00:35 -0500

    First vue project, todo list to get the basics

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Todos />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Todos from "./components/Todos";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Todos,
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          value: "Do One",
+          completed: false,
+        },
+        {
+          id: 2,
+          value: "Do Two",
+          completed: false,
+        },
+        {
+          id: 3,
+          value: "Done Three",
+          completed: true,
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*::before,
+*::after,
+* {
+  padding: 0;
+  margin: 0;
 }
 </style>
